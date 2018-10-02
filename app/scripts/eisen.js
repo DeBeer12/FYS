440d28b6dc9405d0680a603823de3cd33c4fc18d
Jesse van Bree <jessevanbree@live.nl>
2018-10-02 12:45:38 +0200

    Begonnen met feedback fase1 verwerken op functionele eisen pagina

---
+++ app/scripts/eisen.js
@@ -0,0 +1,18 @@
+// $(document).ready(function () {
+//     var eisen = [
+//         "Er moet een profielpagina zijn voor de gebruikers",
+//         "Gebruikers moeten hun profiel kunnen bewerken",
+//         "Er moet een match systeem aanwezig zijn die gebruikers met elkaar koppelt op basis van hun vakantie en interesses",
+//         "Elke gebruiker kan meerdere interesses selecteren die uit een static list komen",
+//         "Voor elke gebruiker moet een list met matches gegenereerd worden op basis van hun vakantie en interesses",
+//         "Elke gebruiker kan 1 of meerdere vakanties opgeven",
+//         "Het systeem kan mails sturen voor het aanmelden van een nieuwe gebruiker of nieuwe matches",
+//         "Er moet een chatsysteem aanwezig zijn voor de gebruikers",
+//         "Er zijn moderator tools aanwezig met meer rechten",
+//         "Er moet een CMS aanwezig zijn"
+//     ];
+//
+//     for (var i = 0; i < eisen.length; i++) {
+//         console.log(eisen[i])
+//     }
+// });
