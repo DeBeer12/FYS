0c5f77151fb5596efaf4621369d1eeaa546810e6
Jesse van Bree <jessevanbree@live.nl>
2018-10-11 10:57:41 +0200

    mockup matchpagina af

--- fase_2_mockup/scripts/my_matches.js
+++ fase_2_mockup/scripts/my_matches.js
@@ -1,6 +1,9 @@
-$( document ).ready(function() {
-    console.log("Ready set Go!!!");
+var match_template = '<div class="match"> <div class="match_picture"> <img src="images/img_avatar.png" alt="Match Picture"> </div><div class="match_info"> <p class="match_name">John Doe</p><p class="match_age">21 Jaar</p><p class="match_date">1 week geleden gematched</p></div><div class="match_tags_container"> <ul class="match_tags"> Vakanties gepland: <li class="match_tag">Lorem ipsum</li><li class="match_tag">Lorem ipsum</li></ul> </div><div class="match_icons"> <i id="chat_with_match" class="fas fa-2x fa-comments"></i> <i id="delete_match" class="fas fa-2x fa-trash"></i> </div></div>'
 
+$( document ).ready(function() {
+    for(var i = 0; i < 10;i++){
+        $('.container').append(match_template);
+    }
     $('#delete_match').click(function(elem){
         deleteMatch();
     });
