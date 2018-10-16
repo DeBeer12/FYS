d2e422af3a1cdbb1490441e79048fc0f619270de
Unknown <mikefels@hotmail.com>
2018-10-16 23:07:57 +0200

    Home progress "not finished"

--- fase_2_mockup/scripts/home.js
+++ fase_2_mockup/scripts/home.js
@@ -5,27 +5,27 @@ String.prototype.replaceAll = function (search, replacement) {
 
 var users = [{
     id: "1",
-    name: "Jhon Doe",
-    description: "Groetjes"
+    name: "User 1",
+    description: "Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed."
 }, {
     id: "2",
-    name: "Loekie Yoekie",
-    description: "Yo"
+    name: "User 2",
+    description: "Sed consequat, leo eget bibendum sodales, augue velit cursus nunc, quis gravida magna mi a libero. Fusce vulputate eleifend."
 }, {
     id: "3",
-    name: "Klaas Vaak",
-    description: "Gaatie maat"
+    name: "User 3",
+    description: "Vestibulum purus quam, scelerisque ut, mollis sed, nonummy id, metus. Nullam accumsan lorem in dui, Cras ultricies mi eu turpis."
 }, {
     id: "4",
-    name: "Joost Proost",
-    description: "Biertje"
+    name: "User 4",
+    description: "Pitcher voor weinig"
 }, {
     id: "5",
-    name: "Piet Biet",
-    description: "Biertje"
+    name: "User 5",
+    description: "Heeft het niet"
 }, {
     id: "6",
-    name: "Jap Pap",
+    name: "User 6",
     description: "Neeeeee"
 }];
 
@@ -33,14 +33,32 @@ var users = [{
 $(document).ready(function () {
     var carouselItemTemplate = $('div#carousel-item-template').parent().html();
 
-    for (var i = 0; i < 3; i++) {
+    $('div#carousel-item-template').parent().empty();
+
+    for (var i = 0; i <= 2; i++) {
         newCarouselItem = (' ' + carouselItemTemplate).slice(1);
         newCarouselItem = newCarouselItem.replace("{{name}}", users[i].name)
             .replace("{{description}}", users[i].description);
         $(".flex-wrapper").append(newCarouselItem);
-        $("." + users[i].id).removeClass("user-card-wrapper-display-none")
-        console.log($("." + users[i].id));
-        console.log($('div#carousel-item-template').prev());
-    }
+        $("." + users[i].id).removeClass("user-card-wrapper-display-none");
+        $('#carousel-item-template').attr('id', "template-id-" + users[i].id).removeClass("user-card-wrapper-display-none");
 
+        $("#template-id-" + i + " .user-text-container .user-button #removeItem").on('click', function(){
+            var answer = confirm("Wilt u gebruiker " + users[i].name + " negeren?");
+            if (answer) {
+                $("#template-id-" + i).remove();
+            }
+        });
+
+        $("#template-id-" + i + " .user-text-container .user-button #matchItem").on('click', function(){
+
+            var answer = confirm("Wilt u met " + users[i].name + " matchen?");
+            if (answer) {
+                $("#template-id-" + i).remove();
+            }
+        });
+    }
 });
+
+// wanneer er op die knop wordt gedrukt, kijk je welk id als laatste word ingeladen met een if statement vervolgens .empty() je alles in de flex-wrapper
+// en loop je door de array met een if statement die alleen door laat als het id hoger is dan de laatste id die je eerder al ophaald.
--- fase_2_mockup/scripts/my_matches.js
+++ fase_2_mockup/scripts/my_matches.js
@@ -5,7 +5,27 @@ String.prototype.replaceAll = function(search, replacement) {
 };
 
 // Template for match
-var match_template = '<div id="match{{id}}" class="match"> <div class="match_picture"> <img src="images/img_avatar.png" alt="Match Picture"> </div><div class="match_info"> <p class="match_name">John Doe</p><p class="match_age">21 Jaar</p><p class="match_date">1 week geleden gematched</p></div><div class="match_tags_container"> <ul class="match_tags"> Vakanties gepland: <li class="match_tag">Lorem ipsum</li><li class="match_tag">Lorem ipsum</li></ul> </div><div class="match_icons"> <a href="chat.html" title="Chat with match {{id}}"><i id="chat_with_match" class="fas fa-2x fa-comments"></i></a> <i id="delete_match" onclick="deleteMatch({{id}})" class="fas fa-2x fa-trash"></i> </div></div>'
+var match_template = '<div id="match{{id}}" class="match"> ' +
+    '<div class="match_picture"> ' +
+    '<img src="images/img_avatar.png" alt="Match Picture"> </div>' +
+    '<div class="match_info"> ' +
+    '<p class="match_name">John Doe</p>' +
+    '<p class="match_age">21 Jaar</p>' +
+    '<p class="match_date">1 week geleden gematched</p>' +
+    '</div>' +
+    '<div class="match_tags_container"> ' +
+    '<ul class="match_tags"> Vakanties gepland: ' +
+    '<li class="match_tag">Lorem ipsum</li>' +
+    '<li class="match_tag">Lorem ipsum</li>' +
+    '</ul> ' +
+    '</div>' +
+    '<div class="match_icons"> ' +
+    '<a href="chat.html" title="Chat with match {{id}}">' +
+    '<i id="chat_with_match" class="fas fa-2x fa-comments">' +
+    '</i>' +
+    '</a> ' +
+    '<i id="delete_match" onclick="deleteMatch({{id}})" class="fas fa-2x fa-trash"></i> ' +
+    '</div></div>'
 
 $(document).ready(function() {
     // Show insert match template 10 times in container
