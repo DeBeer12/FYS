881439400da98a420cb1c36d1666de7610e50dd8
bob broersen <broersb3@hva.nl>
2018-10-16 14:16:50 +0200

    fixed it!! profile page werkt nu met aanpassen

--- fase_2_mockup/scripts/profile-checkbox.js
+++ fase_2_mockup/scripts/profile-checkbox.js
@@ -16,52 +16,27 @@ $(document).ready(
 		});
 
 		$(".edit-image").click(function () {
-			document.getElementById("edit-svg").style.display = "none";
-			document.getElementById("save-svg").style.display = "block";
+			$( "div.profile" ).toggleClass( "hide" )
 		    var parentElms = document.getElementById("profile");
 		    var elms = parentElms.getElementsByTagName("p");
-		    for(var i=0; elms.length > i; i++){	    
+		    var elmsTxt = parentElms.getElementsByTagName("textarea");
 
-				// Hide it
-		        elms[i].style.display = "none";
+		    for(var i = 0; elmsTxt.length > i; i++){
+		    	elmsTxt[i].value = elms[i].innerHTML;
+		    }
 
-		        // Get its text
-		        text = elms[i].innerHTML;
-
-		       	splitted = text.split(".");
-		        var countPeriod = splitted.length;
-					console.log(countPeriod);
-
-					checkBigger = 0;
-
-					for (var e = 0; splitted.length > e; e++) {
-						if (splitted[e].length >= checkBigger) {
-							checkBigger = splitted[e];
-						}
-						else{
-							break;
-						}
-					}
-					console.log(checkBigger.length);
-
-		        // Create an input
-		        input = document.createElement("textarea");
-		        input.value = text;
-		        input.rows = countPeriod + 1;
-		        input.cols = checkBigger.length + 8;
-		        input.style.resize = "none";
-		        elms[i].parentNode.insertBefore(input, elms[i]);
-	    	}
 		});   
 
 		$(".save-image").click(function () {
 			var parentElmsP = document.getElementById("profile");
-		    var elmsP = parentElms.getElementsByTagName("p");
+		    var elmsP = parentElmsP.getElementsByTagName("p");
 
 			var parentElmsTxt = document.getElementById("profile");
-		    var elmsTxt = parentElms.getElementsByTagName("textarea");
-
-		    
+		    var elmsTxt = parentElmsTxt.getElementsByTagName("textarea");
+		    for(var t=0; elmsP.length > t; t++){	
+		    	elmsP[t].innerHTML = elmsTxt[t].value;
+		    }
+			$( "div.profile" ).toggleClass( "hide" )
 
 		});
 	}
