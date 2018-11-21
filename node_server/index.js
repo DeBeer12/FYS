87d3bfb48346b29b7e0d2c1573a0104f4fa74431
Danny Huigen <link3321@hotmail.com>
2018-11-21 15:27:32 +0100

    Creating templating enine for node

--- node_server/index.js
+++ node_server/index.js
@@ -5,22 +5,25 @@ var http = require("http");
 var app = express();
 
 app.get('/*', function (req, res) {
-    // res.send('Hello World');
-
-    var fullUrl = "fase_2_mockup" + req.originalUrl + ".html";
-    console.log(fullUrl);
-
+    var fullUrl = "fase_2_mockup" + req.originalUrl;
     fs.readFile(fullUrl ,function (err, data){
-        res.writeHead(200, {'Content-Type': 'text/html','Content-Length':data.length});
-        res.write(data);
-        res.end();
+        if (err){
+            res.write("404");
+            res.end();
+        }
+        else {
+            if(req.originalUrl.includes(".html")){res.writeHead(200, {'Content-Type': 'text/html','Content-Length':data.length});}
+            else if(req.originalUrl.includes(".css")){res.writeHead(200, {'Content-Type': 'text/css'});}
+            else if(req.originalUrl.includes(".js")){res.writeHead(200, {'Content-Type': 'text/javascript'});}
+            res.write(data);
+            res.end();
+        }
     });
-
 });
+
 
 var server = app.listen(8081, function () {
     var host = server.address().address;
     var port = server.address().port;
-
     console.log("Example app listening at http://%s:%s", host, port)
 });
