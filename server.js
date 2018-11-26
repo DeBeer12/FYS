8a27ea10f8978009f797e1aa8d3f4596be757af0
Jesse van Bree <jessevanbree@live.nl>
2018-11-26 13:58:41 +0100

    cleaned up server.js

--- server.js
+++ server.js
@@ -1,29 +1,31 @@
+// App dependencies
 var fs = require("fs");
 var express = require("express");
 var mysql = require('mysql');
 
+// Config files
 var config = require("./appsettings.json")
 
+// intitalize the app
 var app = express();
+var db = mysql.createConnection(config.databaseRemote);
 
+// Request handler
 app.get('/*', function (req, res) {
-
     var fullUrl = "public" + req.originalUrl;
 
-    if(fullUrl === "public/"){
+    if (fullUrl === "public/") {
         fullUrl = "public/index.html";
     }
 
-    fs.readFile(fullUrl ,function (err, data){
-        if (err){
+    fs.readFile(fullUrl, function (err, data) {
+        if (err) {
             res.write("404");
             res.end();
-        }
-        else {
-            if(req.originalUrl.includes(".html"))
-            {res.writeHead(200, {'Content-Type': 'text/html','Content-Length':data.length});}
-            else if(req.originalUrl.includes(".css")){res.writeHead(200, {'Content-Type': 'text/css'});}
-            else if(req.originalUrl.includes(".js")){res.writeHead(200, {'Content-Type': 'text/javascript'});}
+        } else {
+            if (req.originalUrl.includes(".html")) {res.writeHead(200, {'Content-Type': 'text/html', 'Content-Length': data.length});}
+            else if (req.originalUrl.includes(".css")) {res.writeHead(200, {'Content-Type': 'text/css'});}
+            else if (req.originalUrl.includes(".js")) {res.writeHead(200, {'Content-Type': 'text/javascript'});}
             res.write(data);
             res.end();
         }
@@ -31,19 +33,15 @@ app.get('/*', function (req, res) {
 
 });
 
-var server = app.listen(8081, function () {
+// Start the server 
+var server = app.listen(8081, function (err) {
+    if(err) throw err;
     var host = server.address().address;
     var port = server.address().port;
-    console.log("Fys server runnng at http://%s:%s", host, port)
-});
-
-var con = mysql.createConnection(config.databaseRemote);
-  
-  con.connect(function(err) {
-    if (err) throw err;
-    console.log("Connected!");
-    con.query("SELECT * FROM user", function (err, result) {
+    console.log("Fys server running at http://%s:%s", host, port)
+    
+    db.connect(function (err, r) {
         if (err) throw err;
-        console.log("Result: " + JSON.stringify(result, null, 4));
+        console.log("Connected to %s as %s", config.databaseRemote.database, config.databaseRemote.user);
     });
-  });
+});
