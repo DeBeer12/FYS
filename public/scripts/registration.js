5a6d8ec533d15e23f7b1816212359ac79fbdcedc
Richard <richardvdv105@gmail.com>
2018-11-27 13:29:23 +0100

    Registratie Af/Done

--- public/scripts/registration.js
+++ public/scripts/registration.js
@@ -1,6 +1,4 @@
-
-
-$("#js-registration-form").submit(function(e)){
+$("#js-registration-form").submit(function(e){
 e.preventDefault();
 
 var first_name = $("#js-registration-form #first_name").val();
@@ -10,5 +8,13 @@ var email = $("#js-registration-form #email").val();
 var password = $("#js-registration-form #password").val();
 var birthday = $("#js-registration-form #birthday").val();
 
-alert(first_name + " " + last_name + " " + user_name + " " + email + " " + password + " " + password + " " + birthday);
-}
+var query = "INSERT INTO user(user_firstname, user_mail, user_password, user_birthday, user_lastname, user_name, user_last_login, role_role_id)" +
+"VALUES('" + first_name + "', '" + email + "', '" + password + "', '" + birthday + "', '" + last_name + "' , '" + user_name + "',now()," + 2 + ");";
+
+console.log(query);
+
+$.get( "/db", {query:query}).done(function( data ) {
+        alert("Geregistreerd!!!!!!!");
+    });
+
+});
