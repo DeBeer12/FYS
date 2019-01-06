cb1e9d6479af792d4ad28bb3be4fe7edf404feb8
Mike Fels <mikefels@hotmail.com>
2019-01-06 15:53:58 +0100

    Updated index page

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -3,137 +3,112 @@ String.prototype.replaceAll = function (search, replacement) {
     return target.replace(new RegExp(search, 'g'), replacement);
 };
 
-var users;
-var title;
-var text;
-var userId;
-
-function loggedInUserId() {
-
-    if ($user != null) {
-        userId = $user.user_id;
-    }
-}
-
-setTimeout(loggedInUserId, 100);
-
-$(document).ready(async function () {
-
-    users = await resolveAfter2Seconds();
-
-
-    // Carousel template
-    var carouselItemTemplate = $('div#carousel-item-template').parent().html();
-
-    // Users where the id already got used for display
-    var userInArray = [];
-
-    // clear template from page
-    $('div#carousel-item-template').parent().empty();
-
-    for (var i = 0; i <= 2; i++) {
-        // pass user id to array
-        userInArray.push(users[i].user_id);
-
-        // Add new carousel wih data from users array
-        newCarouselItem = (' ' + carouselItemTemplate).slice(1);
-        newCarouselItem = newCarouselItem.replace("{{name}}", users[i].user_firstname + " " + users[i].user_lastname)
-            .replace("{{description}}", users[i].user_about);
-        $(".flex-wrapper").append(newCarouselItem);
-        $("." + users[i].user_id).removeClass("user-card-wrapper-display-none");
-        $('#carousel-item-template').attr('id', "template-id-" + users[i].user_id).removeClass("user-card-wrapper-display-none");
+$(document).ready(function () {
+    function authUser(callback){
+        var authUserQuery ="SELECT * FROM user WHERE user.user_id =" + $user.user_id + ";";
+        $.get("/db", {query:authUserQuery}).done(function(data){
+            callback(data[0]);
+        });
     }
 
-    // Match user
-    $(".user-buttons #matchItem").on('click', function () {
-        var templateId = $(this).parent().parent().parent().attr('id');
-        var idArr = templateId.split('-');
-        var answer = confirm("Wilt u gebruiker " + users[idArr[2] - 51].user_firstname + " " + users[idArr[2] - 51].user_lastname + " matchen?"); // 51 is hardcoded to get first user fixing this later
+    function getUsers(callback){
+        var getUsersQuery = "SELECT * FROM user WHERE user_id NOT IN(SELECT user_user_id_liked FROM liked WHERE user_user_id_has_liked ="+ $user.user_id + ") AND NOT user_id ="+ $user.user_id;
+        $.get("/db", {query:getUsersQuery}).done(function (data) {
+            callback(data);
+        });
+    }
 
-        if (answer) {
-            // Get last element from array - 50 to return id usable for collecting new user from users array
-            var lastEl = userInArray.slice(-1)[0] - 50;
-            // Push the user id in userInArray
-            userInArray.push(users[lastEl].user_id);
+    getUsers(function (data) {
+       console.log(data);
 
-            // Remove user from page
-            $("#template-id-" + idArr[2]).fadeOut("slow");
+        // Carousel template
+        var carouselItemTemplate = $('div#carousel-item-template').parent().html();
 
-            // Add new carousel with data from users array
+        // Array for listed users
+        var userInArray = [];
+
+        // clear template from page
+        $('div#carousel-item-template').parent().empty();
+
+        for (var i = 0; i <= 2; i++) {
+            // pass user id to array
+            userInArray.push(data[i].user_id);
+
+            // Add new carousel wih data from users array
             newCarouselItem = (' ' + carouselItemTemplate).slice(1);
-            newCarouselItem = newCarouselItem.replace("{{name}}", users[lastEl].user_firstname + " " + users[lastEl].user_lastname)
-                .replace("{{description}}", users[lastEl].user_about);
+            newCarouselItem = newCarouselItem.replace("{{name}}", data[i].user_firstname + " " + data[i].user_lastname)
+                .replace("{{description}}", data[i].user_about)
+                .replace("{{image}}", "<img id='theImg' src='images/img_avatar.png' style='width: 150px; height: 100%;'/>");
             $(".flex-wrapper").append(newCarouselItem);
-            $("." + users[lastEl].user_id).removeClass("user-card-wrapper-display-none");
-            $('#carousel-item-template').attr('id', "template-id-" + users[lastEl].user_id).removeClass("user-card-wrapper-display-none");
-
-            // // Create match
-            // var query = "INSERT INTO liked (user_user_id_has_liked, user_user_id_liked, like_created_at) values(" + userId + ", " + idArr[2] + ", NOW());"
-            // $.get("/db", {query: query}).done(function (data) {
-            //     alert("Matched");
-            // });
-
+            $("." + data[i].user_id).removeClass("user-card-wrapper-display-none");
+            $('#carousel-item-template').attr('id', "template-id-" + data[i].user_id).removeClass("user-card-wrapper-display-none");
         }
-    });
 
-    // Ignore user
-    $(".user-buttons #removeItem").on('click', function () {
-        var templateId = $(this).parent().parent().parent().attr('id');
-        var idArr = templateId.split('-'); // idArr[2] returns the id of selected user
-        var answer = confirm("Wilt u gebruiker " + users[idArr[2] - 51].user_firstname + " " + users[idArr[2] - 51].user_lastname + " negeren?"); // 51 is hardcoded to get first user fixing this later
+        // Ignore user
+        $(document).on("click", ".user-buttons #removeItem", function(){
+            var templateId = $(this).parent().parent().parent().attr('id');
+            var idArr = templateId.split('-'); // idArr[2] returns the id of selected user
+            var answer = confirm("Wilt u gebruiker " + data[idArr[2] - 51].user_firstname + " " + data[idArr[2] - 51].user_lastname + " negeren?"); // Not finished yet ***
 
-        if (answer) {
-            // Get last element from array - 50 to return id usable for collecting new user from users array
-            var lastEl = userInArray.slice(-1)[0] - 50;
-            // Push the user id in userInArray
-            userInArray.push(users[lastEl].user_id);
+            if (answer) {
+                // Get last element from array - 50 to return id usable for collecting new user from users array
+                var lastEl = userInArray.slice(-1)[0] - 50;
 
-            // Remove user from page
-            $("#template-id-" + idArr[2]).fadeOut("slow");
+                // Push the user id in userInArray
+                userInArray.push(data[lastEl].user_id);
 
-            // Add new carousel with data from users array
-            newCarouselItem = (' ' + carouselItemTemplate).slice(1);
-            newCarouselItem = newCarouselItem.replace("{{name}}", users[lastEl].user_firstname + " " + users[lastEl].user_lastname)
-                .replace("{{description}}", users[lastEl].user_about);
-            $(".flex-wrapper").append(newCarouselItem);
-            $("." + users[lastEl].user_id).removeClass("user-card-wrapper-display-none");
-            $('#carousel-item-template').attr('id', "template-id-" + users[lastEl].user_id).removeClass("user-card-wrapper-display-none");
-        }
-    });
+                // Remove user from page
+                $("#template-id-" + idArr[2]).fadeOut("slow");
 
-    // Title
-    $("#preface-title").replace("{{title}}", title);
+                // Add new carousel with data from users array
+                newCarouselItem = (' ' + carouselItemTemplate).slice(1);
+                newCarouselItem = newCarouselItem.replace("{{name}}", data[lastEl].user_firstname + " " + data[lastEl].user_lastname)
+                    .replace("{{description}}", data[lastEl].user_about);
+                $(".flex-wrapper").append(newCarouselItem);
+                $("." + data[lastEl].user_id).removeClass("user-card-wrapper-display-none");
+                $('#carousel-item-template').attr('id', "template-id-" + data[lastEl].user_id).removeClass("user-card-wrapper-display-none");
+            }
+        });
 
-    // Text
-    $("#preface-text-area").replace("{{content}}", text);
+        // Match user
+        $(document).on("click", ".user-buttons #matchItem", function(){
+            var templateId = $(this).parent().parent().parent().attr('id');
+            var idArr = templateId.split('-');
+            var answer = confirm("Wilt u gebruiker " + data[idArr[2] - 51].user_firstname + " " + data[idArr[2] - 51].user_lastname + " matchen?"); // 51 is hardcoded to get first user fixing this later
 
-});
+            if (answer) {
+                // Get last element from array - 50 to return id usable for collecting new user from users array
+                var lastEl = userInArray.slice(-1)[0] - 50;
+                // Push the user id in userInArray
+                userInArray.push(data[lastEl].user_id);
 
-function resolveAfter2Seconds() {
-    return new Promise(resolve => {
-        $.get("/db", {query: "SELECT * FROM fys_is106_1.user"}).done(function (data) {
-            resolve(data);
+                // Remove user from page
+                $("#template-id-" + idArr[2]).fadeOut("slow");
+
+                // Create match
+                var createMatchQuery = "INSERT INTO liked (user_user_id_has_liked, user_user_id_liked, like_created_at) values(" + $user.user_id + ", " + idArr[2] + ", NOW());";
+                $.get("/db", {query: createMatchQuery}).done(function () {});
+
+                // Add new carousel with data from users array
+                newCarouselItem = (' ' + carouselItemTemplate).slice(1);
+                newCarouselItem = newCarouselItem.replace("{{name}}", data[lastEl].user_firstname + " " + data[lastEl].user_lastname)
+                    .replace("{{description}}", data[lastEl].user_about);
+                $(".flex-wrapper").append(newCarouselItem);
+                $("." + data[lastEl].user_id).removeClass("user-card-wrapper-display-none");
+                $('#carousel-item-template').attr('id', "template-id-" + data[lastEl].user_id).removeClass("user-card-wrapper-display-none");
+            }
         });
     });
-}
 
-function getIndexTextTitle() {
-    return new Promise(resolve => {
-        $.get("/db", {query: "SELECT * FROM fys_is106.content WHERE content_location = 'index_title'"}).done(function (data) {
-            resolve(data);
-        })
+    authUser(function(data){
+       if(data.role_role_id = 1){
+           $('#preface-edit').show();
+       }
     });
-}
+});
 
-function getIndexTextContent() {
-    return new Promise(resolve => {
-        $.get("/db", {query: "SELECT * FROM fys_is106.content WHERE content_location = 'index_text'"}).done(function (data) {
-            resolve(data);
-        })
-    });
-}
-
-$('#preface-edit').on('click', function () {
+// $('#preface-edit').on('click', function () {
+$(document).on("click", "#preface-edit", function () {
     $('#preface-title').replaceWith("<h2 id='preface-title-edit-1'>Titel:</h2> <input type='text' id='preface-title-edit-2'><br>");
     $('#preface-text-area').replaceWith("<p id='preface-text-area-edit-1'>Info:</p> <textarea rows='7' cols='65' id='preface-text-area-edit-2'></textarea><br>");
     $('#preface-save').append('<i class="far fa-save fa-2x pointer" id="preface-save-icon"></i>');
